0ec37861c056636a0dff9fc99312cfcb2a91d0f9
Muhammed Rashid <raashxi@gmail.com>
2026-06-25 10:55:19 +0530

    Completed Function & JS Rounding()

---
+++ 12.0 javaScript/getMilk.js
@@ -0,0 +1,20 @@
+function getMilk(money) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("I got " + (Math.floor(money / 1.5)) + " bottle of milk.");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(5); // a bottle of milk cost 1.5 dollars 
