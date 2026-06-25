5754f641b67caba1cb2da9dbfbfe8d8d3a6a4f9e
Muhammed Rashid <raashxi@gmail.com>
2026-06-25 11:56:33 +0530

    Completed ageWeeks exercises in Best Practice

--- 12.0 javaScript/ageWeeks.js
+++ 12.0 javaScript/ageWeeks.js
@@ -3,11 +3,13 @@ function lifeInWeeks(age) {
 /************Don't change the code above************/    
     
     //Write your code here.
-    var daysUntil90 = 365 * 90;
-    var weeksUntil90 = 52 * 90;
-    var monthsUntil90 = 12 * 90;
+    const maxAge = 90;
     
-    console.log("You have " + (daysUntil90 - (age * 365)) + " days, " + (weeksUntil90 - (age * 52)) + " weeks, and " + (monthsUntil90 - (age * 12)) + " months left.");
+    const daysLeft = ( maxAge - age ) * 365;
+    const weeksLeft = (maxAge - age ) * 52;
+    const monthsLeft = (maxAge - age) * 12;
+    
+    console.log(`You have ${daysLeft} days, ${weeksLeft} weeks, and ${monthsLeft} months left.`)
     
     
 /*************Don't change the code below**********/
