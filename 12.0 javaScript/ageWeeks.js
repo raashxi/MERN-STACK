4f322aeb3760c13fee82c6684a6910713403dd6f
Muhammed Rashid <raashxi@gmail.com>
2026-06-25 11:38:59 +0530

    Completed Exercise of ageWeeks

---
+++ 12.0 javaScript/ageWeeks.js
@@ -0,0 +1,14 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var daysUntil90 = 365 * 90;
+    var weeksUntil90 = 52 * 90;
+    var monthsUntil90 = 12 * 90;
+    
+    console.log("You have " + (daysUntil90 - (age * 365)) + " days, " + (weeksUntil90 - (age * 52)) + " weeks, and " + (monthsUntil90 - (age * 12)) + " months left.");
+    
+    
+/*************Don't change the code below**********/
+}
