0a1e401c607587a1dfe0047a9b11f65d94301be9
Muhammed Rashid <raashxi@gmail.com>
2026-06-26 09:41:37 +0530

    Completed love calc

---
+++ 12.0 javaScript/loveCalc.js
@@ -0,0 +1,9 @@
+
+
+prompt("What's your name.");
+prompt("What's your partner's name.");
+
+var love = Math.random() * 100 ;
+love = Math.floor(love)+1;
+console.log(`Your love percentage is ${love}%`);
+
